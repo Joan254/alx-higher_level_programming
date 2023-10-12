6e3f016d61737d21ddbfe2d0db433f547103e501
Joan254 <joanotieno1998@gmail.com>
2023-10-12 12:23:48 +0000

    rectangle.py updated

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,8 @@ class Rectangle(Base):
             for column in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """Defines a format for the string representation of the class."""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
+{self.__width}/{self.__height}"
